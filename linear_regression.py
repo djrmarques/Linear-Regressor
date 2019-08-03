91346eb9a5213e451224f8ff8f74c2b6a2bb4ecb
David Marques <david@david-t420>
2019-08-03 20:47:02 +0100

    first commit

---
+++ linear_regression.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# This module performs linear regression on the provided data presents the relevant statistics scores
+# This module was written while following the ISLR book
+
+class LinearRegression:
+    """ Linear Regression """
+
+    def __init__(self):
+        pass
+
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        pass
+    
+    def plot(self):
+        pass
