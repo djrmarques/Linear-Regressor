1413a0a568e30959b67835ce4f79b6f4c855af60
David Marques <david@david-t420>
2019-08-04 10:10:28 +0100

    Fixed some errors

--- linear_regression.py
+++ linear_regression.py
@@ -10,6 +10,29 @@ class LinearRegression:
         """ Determined the coeficients for the regression """
         pass
 
+
+    def _single_fit(self, X, y):
+        """ This method fits a simple linear regression (Only one indepent variable)"""
+
+        self._inputs_check(X, y)
+
+        # Assert that the training set only has one variable
+        n_independent = X.ndim
+        assert n_independent == 1, f"The number of indepent variables is {n_independent} and should be just one."
+
+        X_mean = X.mean()
+        y_mean = y.mean()
+
+        beta1 = ((X - X_mean)*(y - y_mean)).sum()/(np.sum(X-X_mean)**2)
+        beta0 = y_mean - beta1*X_mean
+
+        print(beta0, beta1)
+
+
+    def predict(self, X):
+        """ Makes predictions on a test data set"""
+        pass
+
     @staticmethod
     def _inputs_check(X, y):
         """ Checks if the provided inputs are in the right format """
@@ -23,26 +46,8 @@ class LinearRegression:
         assert np.isnan(y).sum() == 0, "There are missing values in y"
 
         # Check the shape of the inputs
+        assert y.ndim == 1, f"y array should have dimention one, not {y.dim}"
         assert X.shape[0] == y.shape[0], "The dependent and independt variables do not have the same shape."
-
-    def _single_fit(self, X, y):
-        """ This method fits a simple linear regression (Only one indepent variable)"""
-
-        self._inputs_check(X, y)
-
-        # Assert that the training set only has one variable
-        n_independent = X.shape[1]
-        assert n_independent == 1, f"The number of indepent variables is {n_independent} and should be just one."
-
-
-    def predict(self, X):
-        """ Makes predictions on a test data set"""
-        pass
-
-    def fit_single_linear_regression(self):
-        """ Performs a fit method"""
-        pass
-
 
 # The parentehsis are just because of EMACS
 if (__name__ == "__main__"):
@@ -51,10 +56,9 @@ if (__name__ == "__main__"):
 
     datapath = "../islr/datasets/Advertising.csv"
     df = pd.read_csv(datapath, index_col=0)
-    print(df.head())
 
-    X = df.drop("sales", axis=1).values
+    X = df["TV"].values
     y = df["sales"].values
 
     lr = LinearRegression()
-    lr.fit(X, y)
+    lr._single_fit(X, y)
