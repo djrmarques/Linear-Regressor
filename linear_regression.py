2c551f216ade2f3c4a549803e5f9442a97ba7565
David Marques <david@david-t420>
2019-08-04 13:29:13 +0100

    r2 now calculed correctly

--- linear_regression.py
+++ linear_regression.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 # This module performs linear regression on the provided data presents the relevant statistics scores
 # This module was written while following the ISLR book
+
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 class LinearRegression:
     """
@@ -11,16 +14,27 @@ class LinearRegression:
 
     def __init__(self):
         self.coeficients = None
+        self.r2 = None
+        self._polyfit = None
+        self.prediction = None
+
+        self.rss = None
+        self.tss = None
+
+        self.label = None
+
 
     def fit(self, X, y):
         """ Determined the coeficients for the regression """
         pass
 
-
     def _single_fit(self, X_train, y):
         """ This method fits a simple linear regression, and gets the parameters (Only one indepent variable) """
 
         self._inputs_check(X_train, y)
+
+        self.y = y  # TODO: Change this later
+        self.X = X  # TODO: Change this later
 
         # Assert that the training set only has one variable
         n_independent = X_train.ndim
@@ -33,14 +47,34 @@ class LinearRegression:
         beta0 = y_mean - beta1*X_train_mean
 
         # Save the coeficients
-        self.coeficients = np.array([beta0, beta1])
+        self.coeficients = np.array([beta1, beta0])
+        self._polyfit = np.poly1d(self.coeficients)
+
+    def scatter_matrix(self):
+        """ Plots the scatter matrix with the corresponding linear fit line """
+
+        plt.figure(figsize=(10, 10))
+
+        plt.scatter(self.X, self.y, alpha=0.5)
+        plt.plot(tuple(map(self._polyfit, range(0, self.X.max().astype(int)))), color="red")
+        plt.show()
+
 
     def score(self, X_test):
         """ Scores the obtained on the provided test set. Uses R squared."""
 
-    def predict(self, X):
-        """ Makes predictions on a test data set"""
-        pass
+        assert self.coeficients is not None, "Run the fit method first."
+
+        # Convert array to 2D, if it is only 1D.
+        if X_test.ndim > 1:
+            X_test = X_test.T
+
+        # Evaluate the X_test points
+        self.prediction = np.apply_along_axis(self._polyfit, axis=0, arr=X_test)
+
+        self.rss = np.square(self.y - self.prediction).sum()
+        self.tss = np.square(self.y - self.y.mean()).sum()
+        self.r2 = 1 - self.rss/self.tss
 
     @staticmethod
     def _inputs_check(X, y):
@@ -71,3 +105,5 @@ if (__name__ == "__main__"):
 
     lr = LinearRegression()
     lr._single_fit(X, y)
+    lr.score(X)
+    lr.scatter_matrix()
